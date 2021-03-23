76178d19bc6ad050dfc3f7a4f4e6fa6685d0b376
Romit Dasgupta <romlinux@gmail.com>
2021-03-23 02:10:21 -0700

    Successfully builds

--- FreeRTOS/Demo/CORTEX_M0+_RaspberryPiPico_GCC/common.mk
+++ FreeRTOS/Demo/CORTEX_M0+_RaspberryPiPico_GCC/common.mk
@@ -1,16 +1,13 @@
 CC = arm-none-eabi-gcc
 OBJCOPY = arm-none-eabi-objcopy
 BUILD_DIR := build
-LDFLAGS = -T $(LINKER_SCRIPT) --specs=nosys.specs -nostartfiles
+LDFLAGS = -T $(LINKER_SCRIPT) --specs=nosys.specs 
 LDFLAGS += -Xlinker -Map=${BUILD_DIR}/output.map
 
 CFLAGS += -march=armv6-m -mcpu=cortex-m0plus -mthumb
-CFLAGS += -Wno-error=implicit-function-declaration
-CFLAGS += -Wno-builtin-declaration-mismatch -Werror
 
 ifeq ($(DEBUG), 1)
     CFLAGS += -ggdb3 -Og
 else
     CFLAGS += -O3
 endif
-    CFLAGS += -fstrict-aliasing -Wstrict-aliasing
