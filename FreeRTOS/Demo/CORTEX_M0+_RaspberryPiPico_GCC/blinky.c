7de939a2f00f111751a433a957d96139c2bb9cc0
Romit Dasgupta <romlinux@gmail.com>
2021-03-26 01:51:10 -0700

    preparing to push to mainline

--- FreeRTOS/Demo/CORTEX_M0+_RaspberryPiPico_GCC/blinky.c
+++ FreeRTOS/Demo/CORTEX_M0+_RaspberryPiPico_GCC/blinky.c
@@ -7,11 +7,11 @@ static void prvBlinkyTask( void *pvParameters );
 
 #define LED_PIN 25
 #define blinkyTASK_PRIORITY         ( tskIDLE_PRIORITY + 1 )
-#define TASK_DELAY  500
+#define TASK_DELAY  1000
 int main()
 {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init( LED_PIN );
+    gpio_set_dir( LED_PIN, GPIO_OUT );
     xTaskCreate( prvBlinkyTask,
                  "PicoBlinky",
                  configMINIMAL_STACK_SIZE,
@@ -26,9 +26,9 @@ int main()
 static void prvBlinkyTask( void *pvParameters )
 {
     while(1) {
-        gpio_put(LED_PIN, 1);
-        vTaskDelay(TASK_DELAY);
-        gpio_put(LED_PIN, 0);
-        vTaskDelay(TASK_DELAY);
+        gpio_put( LED_PIN, 1 );
+        vTaskDelay( TASK_DELAY );
+        gpio_put( LED_PIN, 0 );
+        vTaskDelay( TASK_DELAY );
     }
 }
