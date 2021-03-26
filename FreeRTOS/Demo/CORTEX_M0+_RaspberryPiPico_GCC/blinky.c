8e28ba3078ada0195e2bc8714bb84cfaa94b95d8
Romit Dasgupta <romlinux@gmail.com>
2021-03-26 01:30:31 -0700

    Finally a blinking LED

--- FreeRTOS/Demo/CORTEX_M0+_RaspberryPiPico_GCC/blinky.c
+++ FreeRTOS/Demo/CORTEX_M0+_RaspberryPiPico_GCC/blinky.c
@@ -1,12 +1,17 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
+#include "gpio.h"
+
 static void prvBlinkyTask( void *pvParameters );
 
+#define LED_PIN 25
 #define blinkyTASK_PRIORITY         ( tskIDLE_PRIORITY + 1 )
-
+#define TASK_DELAY  500
 int main()
 {
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
     xTaskCreate( prvBlinkyTask,
                  "PicoBlinky",
                  configMINIMAL_STACK_SIZE,
@@ -20,5 +25,10 @@ int main()
 
 static void prvBlinkyTask( void *pvParameters )
 {
-    while(1);   
+    while(1) {
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(TASK_DELAY);
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(TASK_DELAY);
+    }
 }
